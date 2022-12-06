6424f075840acc73dcf1403a6a57b5c3dc22198e
p0c4 <73047020+p0c4@users.noreply.github.com>
2022-12-05 21:10:14 -0800

    Create 2_main.py

---
+++ Day 5/2_main.py
@@ -0,0 +1,28 @@
+l_1 = ["N", "C", "R", "T", "M", "Z", "P"]
+l_2 = ["D", "N", "T", "S", "B", "Z"]
+l_3 = ["M", "H", "Q", "R", "F", "C", "T", "G"]
+l_4 = ["G", "R", "Z"]
+l_5 = ["Z", "N", "R", "H"]
+l_6 = ["F", "H", "S", "W", "P", "Z", "L", "D"]
+l_7 = ["W", "D", "Z", "R", "C", "G", "M"]
+l_8 = ["S", "J", "F", "L", "H", "W", "Z", "Q"]
+l_9 = ["S", "Q", "P", "W", "N"]
+
+dict_l = {1: l_1, 2: l_2, 3: l_3, 4: l_4, 5: l_5, 6: l_6, 7: l_7, 8: l_8, 9: l_9}
+
+with open("puzzle5_data1.txt") as file:
+    data_list = [v.split(" ")[1::2] for v in file.readlines()]
+
+for (x, y, z) in data_list:
+    move_count = int(x)
+    move_from = int(y)
+    move_to = int(z)
+    list_1 = dict_l[move_from]
+    list_2 = dict_l[move_to]
+    list_2.extend(list_1[len(list_1) - move_count:])
+    del list_1[len(list_1) - move_count:]
+    dict_l.update({move_from: list_1, move_to: list_2})
+
+for i in range(9):
+    print(dict_l[i+1][-1])
+
